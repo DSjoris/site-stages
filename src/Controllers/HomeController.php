fa236aeff2bde1417918cf81b112d58cbd882a7c
sasha-meunier <sasha.meunier@viacesi.fr>
2026-03-12 00:32:10 +0100

    feat: mise en place des fonctionnalités liées à la database

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,6 +1,9 @@
 <?php
     namespace App\Controllers;
 
+    use App\Models\MySQLDatabase;
+    use App\Models\OfferModel;
+
     class HomeController extends Controller {
         public function __construct($templateEngine) {
             $this->model = null;
@@ -8,7 +11,17 @@
         }
 
         public function homePage() {
-            echo $this->templateEngine->render('home.html.twig');
+            $db = new MySQLDatabase();
+            $offerModel = new OfferModel($db);
+            $lastsOffers = $offerModel->getLast3Offers();
+
+            foreach($lastsOffers as &$offer) {
+                $offer['skills'] = $offer['skills_list'] ? explode(', ', $offer['skills_list']) : [];
+            }
+
+            echo $this->templateEngine->render('home.html.twig', [
+                'lastOffers' => $lastsOffers
+            ]);
         }
     }
 ?>
