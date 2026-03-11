610771b08af4f50ca023e990eec5fe8a22719283
sasha-meunier <sasha.meunier@viacesi.fr>
2026-03-11 13:58:06 +0100

    feat: ajout des includes pour les répétitions de header et footer

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+    <footer class="footer">
+        <div class="footer-links">
+            <a href="/legal-notices">Mentions légales</a>
+            <hr>
+            <a href="#" onclick="navigator.clipboard.writeText('contact@cstage.fr'); alert('Adresse email copiée.');">Contactez nous</a>
+        </div>
+
+        <span class="footer-copyright">&copy; 2026 C'Stage</span>
+    </footer>
+
+    <script src="./assets/js/index.js"></script>
+    <script src="https://kit.fontawesome.com/c470132ce8.js" crossorigin="anonymous"></script>
+    
+    </body>
+</html>
