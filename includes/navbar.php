714f2a8b1a1f114834b275bc28f705679d6df566
sasha-meunier <sasha.meunier@viacesi.fr>
2026-03-11 13:58:27 +0100

    feat: ajout de la navbar

---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<header class="navbar">
+    <div class="logo">
+        <a href="/index.php">C'Stage</a>
+    </div>
+
+    <div class="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
+    <div class="nav-links">
+        <a href="/offres.php">Offres</a>
+        <a href="/entreprises.php">Entreprises</a>
+        <a href="/login.php">Se connecter</a>
+    </div>
+</header>
