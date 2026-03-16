9d50247c515ddabcb01d88ef6c58917b623acd92
sasha-meunier <sasha.meunier@viacesi.fr>
2026-03-16 17:42:41 +0100

    feat: ajout de la feature d'authentification et déconnexion

---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+    namespace App\Controllers;
+
+    use App\Models\UserModel;
+
+    class AuthController extends Controller {
+        public function __construct($templateEngine) {
+            $this->model = new UserModel();
+            $this->templateEngine = $templateEngine;
+        }
+
+        public function loginAction() {
+            if($_SERVER['REQUEST_METHOD'] === "POST") {
+                $this->login();
+            } else {
+                echo $this->templateEngine->render('login.html.twig');
+            }
+        }
+
+        public function login() {
+            $email = $_POST['email'] ?? null;
+            $password = $_POST['password'] ?? null;
+
+            if(!isset($email) || empty($email)) {
+                echo $this->templateEngine->render('login.html.twig', ['error' => 'Email invalide.']);
+            } else if(!isset($password) || empty($password)) {
+                echo $this->templateEngine->render('login.html.twig', ['error' => 'Mot de passe invalide.']);
+            }
+
+            $user = $this->model->getUser($email);
+            
+            var_dump($user);
+            if(password_verify($password, $user["password"])) {
+                session_start();
+                $_SESSION['user'] = [
+                    'id' => $user['id_account'],
+                    'email' => $user['email'],
+                    'last_name' => $user['last_name'],
+                    'first_name' => $user['first_name'],
+                    'phone' => $user['phone'],
+                    'user_type' => $user['user_type']
+                ];
+                header('Location: /accueil');
+            } else {
+                echo $this->templateEngine->render('login.html.twig', ['error' => 'Email ou mot de passe incorrect.']);
+            }
+        }
+
+        public function logout() {
+            $_SESSION = [];
+            session_destroy();
+            header('Location: /connexion');
+        }
+    }
+?>
